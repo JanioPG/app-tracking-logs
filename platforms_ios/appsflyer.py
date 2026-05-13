b7aef7397443c8258e630f30e79a7fa74fa9461f
Janio Prado Garcia <janio.garcia@edreamsodigeo.com>
2026-05-13 18:14:01 +0200

    update

--- platforms_ios/appsflyer.py
+++ platforms_ios/appsflyer.py
@@ -2,14 +2,17 @@ import io
 import re
 import subprocess
 import json
+from urllib.parse import unquote
 from tools import utils, ui_data
 from interface import show_log, show_program_finished
 
 
 def convert_to_JSON(log: str) -> str:
     try:
-        data = log.replace(':"{', ':{')
-        data = data.replace('}",', '},')
+        data = unquote(log)
+        data = re.sub(r'\"\{', '{', data)
+        data = re.sub(r'\}\"', '}', data)
+        data = re.sub(r'\\\"', '"', data)
         data = json.loads(data)
         return json.dumps(data, indent=4)
 
@@ -21,12 +24,6 @@ def convert_to_JSON(log: str) -> str:
 def appsflyer():
     colors = ui_data.Colors()
     command = utils.COMMAND.FILTER_APPSFLYER.value
-    
-    DECODE_MAP = {
-        "%22": '"', "%7B": "{", "%7D": "}", "%2C": ",", "%5C\\": "", 
-        "%5B": "[", "%5D": "]", "%20": " ", "%3A": ":"
-    }
-    decode_pattern = re.compile("|".join(re.escape(k) for k in DECODE_MAP.keys()))
 
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     RE_CUID = re.compile(r'CustomerUserID:', re.IGNORECASE)
@@ -39,8 +36,6 @@ def appsflyer():
     ) as proc:
         try:
             for line in proc.stdout:
-                line = decode_pattern.sub(lambda m: DECODE_MAP[m.group(0)], line).strip()
-
                 if RE_CUID.search(line, re.IGNORECASE):
                     styled_line = f"{colors.GREEN}{line}{colors.CLOSE}"
                 
